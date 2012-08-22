8bd082f31a1e538b23f528ca3f3a3542a0b297bc
Kjetil Kjernsmo <kjetilk@cpan.org>
2012-08-22 14:13:21 +0000

    Import of KJETILK/RDF-Generator-Void-0.06 from CPAN.
    gitpan-cpan-distribution: RDF-Generator-Void
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         KJETILK/RDF-Generator-Void-0.06.tar.gz
    gitpan-cpan-author:       KJETILK
    gitpan-cpan-maturity:     released

--- lib/RDF/Generator/Void.pm
+++ lib/RDF/Generator/Void.pm
@@ -23,7 +23,7 @@ RDF::Generator::Void - Generate VoID descriptions based on data in an RDF model
 
 =head1 VERSION
 
-Version 0.05_1
+Version 0.06
 
 Note that this is a beta release. It has the core functionality in
 place to create a basic VoID description and what's there should be
@@ -32,7 +32,7 @@ coming up really soon.
 
 =cut
 
-our $VERSION = '0.05_1';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
@@ -413,11 +413,7 @@ Toby Inkster C<< <tobyink@cpan.org> >>
 
 =over
 
-=item * Larger test dataset for more extensive tests.
-
 =item * URI regexps support.
-
-=item * Partitioning based on properties and classes.
 
 =item * Technical features (esp. serializations).
 
@@ -435,7 +431,7 @@ Toby Inkster C<< <tobyink@cpan.org> >>
 
 =item * Set URI space on partitions.
 
-=item * Save the description to files?
+=item * Use L<CHI> to cache?
 
 =back
 
